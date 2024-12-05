c4d804106f90e0d27527dbf2948be52a8bf55238
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2024-12-05 12:28:51 +0100

    Slime movement fix

--- src/movement.c
+++ src/movement.c
@@ -1,8 +1,6 @@
 #include "grim_fetcher.h"
 
-static t_cell  *find_slime_to_kill_player(t_ivec2 *move);
-static t_cell  *get_slime(size_t *i);
-static t_ivec2 get_move(int rng);
+static t_ivec2 get_move(t_cell *cell, int rng);
 static void    move_slime(t_cell *cell, t_ivec2 move);
 
 void move_symbol(t_map *map, t_ivec2 start, t_ivec2 end)
@@ -23,56 +21,13 @@ void move_symbol(t_map *map, t_ivec2 start, t_ivec2 end)
 
 void move_slimes(long dt)
 {
-    static long   ms;
-    static size_t i;
-    t_cell        *slime;
-    t_ivec2       move;
+    static long ms;
+    size_t      i;
+    size_t      j;
+    t_ivec2     move;
+    t_cell      *slime;
 
-    slime = find_slime_to_kill_player(&move);
-    if (slime)
-    {
-        move_slime(slime, move);
-        return;
-    }
-    if (!man.player.steps)
-        return;
-    slime = get_slime(&i);
-    if (!slime)
-        return;
     ms += dt;
-    if (ms < 750)
-        return;
-    ms -= 750;
-    move = get_move((int)dt);
-    move_slime(slime, move);
-    return;
-}
-
-static t_cell *get_slime(size_t *i)
-{
-    t_cell *slime;
-
-    slime = 0;
-    while (man.map.cells[*i].symbol)
-    {
-        if (man.map.cells[*i].symbol == 'B' || man.map.cells[*i].symbol == 'G')
-        {
-            slime = &man.map.cells[*i];
-            break;
-        }
-        ++*i;
-    }
-    if (!man.map.cells[*i].symbol)
-        *i = 0;
-    return slime;
-}
-
-static t_cell *find_slime_to_kill_player(t_ivec2 *move)
-{
-    size_t i;
-    size_t j;
-    t_cell *slime;
-
     i = 0;
     slime = 0;
     while (man.map.cells[i].symbol)
@@ -85,32 +40,49 @@ static t_cell *find_slime_to_kill_player(t_ivec2 *move)
             {
                 if (slime->neighbors[j]->symbol == 'P')
                 {
-                    set_ivec2(move, slime->neighbors[j]->pos.x - slime->pos.x,
+                    set_ivec2(&move, slime->neighbors[j]->pos.x - slime->pos.x,
                         slime->neighbors[j]->pos.y - slime->pos.y);
-                    return slime;
+                    move_slime(slime, move);
                 }
                 ++j;
             }
+            if (man.player.steps && ms >= 1000 && j == 4)
+                move_slime(slime, get_move(slime, (int)dt));
         }
         ++i;
     }
-    return 0;
+    if (ms >= 1000)
+        ms -= 1000;
+    return;
 }
 
-static t_ivec2 get_move(int rng)
+static t_ivec2 get_move(t_cell *cell, int rng)
 {
+    t_ivec2 allowed[4];
+    size_t  i;
+    size_t  j;
     t_ivec2 move;
 
-    rng = rng_minmax(&rng, 1, 4);
-    set_ivec2(&move, 0, 0);
-    if (rng == 1)
-        move.x = -1;
-    else if (rng == 2)
-        move.x = 1;
-    else if (rng == 3)
-        move.y = -1;
-    else if (rng == 4)
-        move.y = 1;
+    i = 0;
+    j = 0;
+    while (j < 4)
+    {
+        if (!cell->neighbors[j]->is_obstacle)
+        {
+            allowed[i].x = cell->neighbors[j]->pos.x - cell->pos.x;
+            allowed[i].y = cell->neighbors[j]->pos.y - cell->pos.y;
+            dprintf(2, "allowed[%ld].x = %d / allowed[%ld].y = %d\n", i, allowed[i].x, i, allowed[i].y);
+            ++i; 
+        }
+        ++j;
+    }
+    if (!i)
+        set_ivec2(&move, 0, 0);
+    else
+    {
+        rng = rng_minmax(&rng, 0, i - 1);
+        set_ivec2(&move, allowed[rng].x, allowed[rng].y);
+    }
     return move;
 }
 
